25c7925c75e544639d7b2e5c3282fab92bc2768d
Amirouche <amirouche@hyper.dev>
2022-09-26 19:57:57 +0200

    blake3: add tests, and fix !

--- letloop/blake3.scm
+++ letloop/blake3.scm
@@ -1,6 +1,9 @@
 #!chezscheme
 (library (letloop blake3)
-  (export blake3 make-blake3 blake3-update! blake3-finalize!)
+  (export blake3 make-blake3 blake3-update! blake3-finalize
+          check~blake3-000
+          check~blake3-001)
+
   (import (chezscheme))
 
   (define libblake3 (load-shared-object "libblake3.so"))
@@ -35,14 +38,27 @@
               (bytevector->pointer bytevector)
               (bytevector-length bytevector)))))
 
-  (define blake3-finalize!
+  (define blake3-finalize
     (let ((func (foreign-procedure* void "blake3_hasher_finalize" void* void* size_t)))
-      (lambda (hasher)
-        (define bytevector (make-bytevector 32))
-        (func hasher (bytevector->pointer bytevector) 32)
+      (lambda (hasher length)
+        (define bytevector (make-bytevector length))
+        (func hasher (bytevector->pointer bytevector) length)
         bytevector)))
 
-  (define (blake3 bytevector)
-    (define hasher (make-blake3))
-    (blake3-update! hasher bytevector)
-    (blake3-finalize! hasher)))
+  (define blake3
+    (lambda (bytevector)
+      (define hasher (make-blake3))
+      (blake3-update! hasher bytevector)
+      (blake3-finalize hasher 32)))
+
+  (define check~blake3-000
+    (lambda ()
+      (assert (bytevector=? (blake3 (string->utf8 "azul dunith"))
+                            (bytevector 147 96 202 209 250 91 234 79 148 175 155 40 42 42 163 180 23 60 5 78 248 205 93 236 132 217 22 253 234 98 73 27)))))
+
+  (define check~blake3-001
+    (lambda ()
+      (let ((blake3 (make-blake3)))
+        (blake3-update! blake3 (string->utf8 "azul dunith"))
+        (assert (bytevector=? (blake3-finalize blake3 16)
+                              (bytevector 147 96 202 209 250 91 234 79 148 175 155 40 42 42 163 180)))))))
