aa388dc814d148f1112bff0f310e69c976863128
Amirouche A. BOUBEKKI <amirouche@bb2k.fr>
2025-09-23 00:07:36 +0200

    binink-compile: remove string-split.

--- src/binink/base.scm
+++ src/binink/base.scm
@@ -373,17 +373,6 @@
                                                 (library-directories))))
       (filter (lambda (root+filepath) (maybe-library-name (pk 'discover (cdr root+filepath)))) root+filepaths)))
 
-  (define (string-split char-delimiter? string)
-    (define (maybe-add a b parts)
-      (if (= a b) parts (cons (substring string a b) parts)))
-    (let ((n (string-length string)))
-      (let loop ((a 0) (b 0) (parts '()))
-        (if (< b n)
-            (if (not (char-delimiter? (string-ref string b)))
-                (loop a (+ b 1) parts)
-                (loop (+ b 1) (+ b 1) (maybe-add a b parts)))
-            (reverse (maybe-add a b parts))))))
-
   (define make-char-predicate
     (lambda (char)
       (lambda (object)
